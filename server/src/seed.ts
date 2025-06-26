ef4361ba59b1d58b7cd78c32db3eb6c46190ab99
Lucas Alves <l.alves001@gmail.com>
2025-06-26 15:18:06 -0300

    chore: create `db:seed` script

---
+++ server/src/seed.ts
@@ -0,0 +1,33 @@
+import { createUser } from "./__tests__/createUser";
+import { database } from "./infra/database";
+
+const seedDatabase = async () => {
+  await database.connect();
+
+  const user1 = await createUser({
+    email: "user1@mail.com",
+    password: "password",
+    balance: 1000,
+  });
+
+  const user2 = await createUser({
+    email: "user2@mail.com",
+    password: "password",
+    balance: 1000,
+  });
+
+  console.log("Database seeded successfully! Here's the user information:");
+  console.log("---");
+  console.log("User 1:");
+  console.log(`  _id: ${user1._id}`);
+  console.log(`  email: ${user1.email}`);
+  console.log("---");
+  console.log("User 2:");
+  console.log(`  _id: ${user2._id}`);
+  console.log(`  email: ${user2.email}`);
+  console.log("Important: For both users, the password is 'password'."); 
+
+  process.exit(0);
+};
+
+seedDatabase();
